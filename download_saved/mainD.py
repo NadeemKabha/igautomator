6c58417ecd534d06dd683c8b62f85ce25012fd04
Nadeem Kabha <nadeem.kabha@gmail.com>
2023-07-08 19:40:19 +0300

    directory

--- download_saved/mainD.py
+++ download_saved/mainD.py
@@ -3,7 +3,7 @@ from .download import download_reels
 from .csv2Lst import LCConvert,CLConvert,clean
 def download_saved(username:str,password:str):
     linksCSV='./links.csv'
-    videoLib='../videoLib'
+    videoLib='./videoLib'
     links=get_saved_links(username,password)
     LCConvert(linksCSV,links)
     mlinks=CLConvert(linksCSV)
